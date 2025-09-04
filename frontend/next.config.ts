a7f7082301dbba827d443c4ee6ce0ba6b7ae2698
guidoasbun <guido@asbun.io>
2025-09-04 13:06:53 -0700

    Vercel mono-repo configuration for GitHub

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    resolveAlias: {
-      "@": "./src",
-    },
-  },
+  /* config options here */
 };
 
 export default nextConfig;
